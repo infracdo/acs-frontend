d3bc2a3609dbfd850e377eaf019209d0e77a2d9e
infracdo <infracdo@gmail.com>
2024-09-19 05:30:10 +0000

    change BE config

--- src/http-config.js
+++ src/http-config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export default axios.create({
-  baseURL: "http://192.168.90.101:8080",
+  baseURL: "http://192.168.90.101:7547",
   headers: {
     "Content-type": "application/json",
   }
