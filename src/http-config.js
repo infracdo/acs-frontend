e45ea77808973e68a3f33d8f220cee8db029a546
prince <prince@apollotech.co>
2024-09-27 17:06:31 +0800

    http update

--- src/http-config.js
+++ src/http-config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export default axios.create({
-  baseURL: "http://localhost:7547",
+  baseURL: "http://192.168.90.101:7547",
   headers: {
     "Content-type": "application/json",
   }
