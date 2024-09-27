525089161ca00b888c6d114d95bd050baa0050cd
princejulapollo <prince@apollotech.co>
2024-09-27 15:43:01 +0800

    update

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://192.168.90.101:8080",
+  baseURL: "http://localhost:7547",
   headers: {
     "Content-type": "application/json",
   }
