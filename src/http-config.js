00c6241ae2633d81f2e68f30dd6a17dfdf9fe7d5
infracdo <infracdo@gmail.com>
2025-02-13 16:09:43 +0800

    Update http-config.js

--- src/http-config.js
+++ src/http-config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export default axios.create({
-  baseURL: "http://192.168.32.14:7547",
+  baseURL: "http://10.244.2.236:7547",
   headers: {
     "Content-type": "application/json",
   }
