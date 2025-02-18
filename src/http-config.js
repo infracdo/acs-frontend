e1e6a6e53b5786974986d986ce3bbe3ce3166bc6
infracdo <infracdo@gmail.com>
2025-02-18 17:41:43 +0800

    Update http-config.js

--- src/http-config.js
+++ src/http-config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export default axios.create({
-  baseURL: "https://acs-backend.k8s.apolloglobal.net/",
+  baseURL: "https://acs-backend.k8s.apolloglobal.net",
   headers: {
     "Content-type": "application/json",
   }
