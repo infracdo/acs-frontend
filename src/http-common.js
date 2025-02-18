3fc1f0996b248cd3c900a5ffdc7ab4fe286e02f6
infracdo <infracdo@gmail.com>
2025-02-18 14:19:47 +0800

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://192.168.32.23:7547",
+  baseURL: "http://192.168.32.21:7547",
   headers: {
     "Content-type": "application/json",
   }
