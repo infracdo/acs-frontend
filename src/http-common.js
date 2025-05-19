69d91544b36b8ba567dce0e0736fb46e08ddacae
infra <infracdo@gmail.com>
2025-05-19 09:49:45 +0800

    add radius

--- src/http-common.js
+++ src/http-common.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://acs-zeep-be-test.apolloglobal.net",
-  headers: {
-    "Content-type": "application/json",
-  },
-  // baseURL: "http://localhost:7547",
+  // baseURL: "https://acs-zeep-be-test.apolloglobal.net",
   // headers: {
   //   "Content-type": "application/json",
   // },
+  baseURL: "http://localhost:7547",
+  headers: {
+    "Content-type": "application/json",
+  },
 });
